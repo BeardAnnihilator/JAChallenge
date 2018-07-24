7252fb710dd8d1eb95309b537799a1212bb1f8e2
Periphanos Alexandre <periphanos.a@gmail.com>
2018-07-24 18:41:36 +1000

    retrieve info from apiary and feed to the logic module introducing newtonsoft.json

---
+++ JobAdderAssignement.Logic/JobProcessor.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using JobAdderAssignement.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobAdderAssignement.Logic
+{
+    public class JobProcessor
+    {
+        List<Job> _jobs;
+        List<Candidate> _candidates;
+        IMapper _iMapper;
+
+        public JobProcessor(List<Job> jobs, List<Candidate> candidates)
+        {
+            _jobs = jobs;
+            _candidates = candidates;
+            _iMapper = config.CreateMapper();
+        }
+
+        public List<ProcessedJob> GetProcessedJobs()
+        {
+            List<ProcessedJob> ret = new List<ProcessedJob>();
+            foreach (var job in _jobs)
+            {
+                ProcessedJob pj = _iMapper.Map<Job, ProcessedJob>(job);
+                pj.Process(_candidates);
+                ret.Add(pj);
+            }
+            return ret;
+        }
+
+        MapperConfiguration config = new MapperConfiguration(cfg => {
+            cfg.CreateMap<Job, ProcessedJob>();
+        });
+    }
+}
--- JobAdderAssignement.Service/ProcessedJobService.svc.cs
+++ JobAdderAssignement.Service/ProcessedJobService.svc.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using JobAdderAssignement.Logic;
+using JobAdderAssignement.Model;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
@@ -14,8 +18,19 @@ namespace JobAdderAssignement.Service
 
         public ProcessedJobsResponse GetProcessedJobs()
         {
-            return null;
-            //todo next
+            ProcessedJobsResponse ret = new ProcessedJobsResponse();
+            List<Job> jobs = null;
+            List<Candidate> cands = null;
+            using (WebClient wc = new WebClient())
+            {
+                var jobsjson = wc.DownloadString(APIURL + "jobs");
+                var candjson = wc.DownloadString(APIURL + "candidates");
+                jobs = JsonConvert.DeserializeObject<List<Job>>(jobsjson);
+                cands = JsonConvert.DeserializeObject<List<Candidate>>(candjson);
+            }
+            JobProcessor jp = new JobProcessor(jobs, cands);
+            ret.ProcessedJobs = jp.GetProcessedJobs();
+            return ret;
         }
     }
 }
