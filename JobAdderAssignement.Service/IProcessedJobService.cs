21ba5f20afc6fb0face4bf4d3b8e951c083536c5
Periphanos Alexandre <periphanos.a@gmail.com>
2018-07-24 18:20:53 +1000

    set up service layer

---
+++ JobAdderAssignement.Service/IProcessedJobService.cs
@@ -0,0 +1,28 @@
+﻿using JobAdderAssignement.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace JobAdderAssignement.Service
+{
+
+    [ServiceContract]
+    public interface IProcessedJobService
+    {
+
+        [OperationContract]
+        ProcessedJobsResponse GetProcessedJobs();
+    }
+
+    [DataContract]
+    public class ProcessedJobsResponse
+    {
+        [DataMember]
+        public List<ProcessedJob> ProcessedJobs { get; set; }
+    }
+
+}
---
+++ JobAdderAssignement.Service/ProcessedJobService.svc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace JobAdderAssignement.Service
+{
+    public class ProcessedJobService : IProcessedJobService
+    {
+        readonly string APIURL = "https://jobadder1.docs.apiary.io/";
+
+        public ProcessedJobsResponse GetProcessedJobs()
+        {
+            return null;
+            //todo next
+        }
+    }
+}
