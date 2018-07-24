8fd3aecc91684b3941e54663a8efeb64871badc2
Periphanos Alexandre <periphanos.a@gmail.com>
2018-07-24 17:17:14 +1000

    model classes matching data

--- JobAdderAssignement.Model/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace JobAdderAssignement.Model
-{
-    public class Class1
-    {
-    }
-}
---
+++ JobAdderAssignement.Model/Job.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JobAdderAssignement.Model
+{
+    public class Job
+    {
+        public int jobId { get; set; }
+        public string name { get; set; }
+        public string company { get; set; }
+        public string skills { get; set; }
+    }
+}
